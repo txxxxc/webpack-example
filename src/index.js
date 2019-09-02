46f0b1cae6ca90bd3aca7ea539b2ba6eb309c5fe
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-09-02 11:01:29 +0900

    done

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import './style.scss';
 
-console.log('hoge hoge');
-
-alart('hi source-map');
-
 ReactDOM.render(<div>hello react</div>, document.getElementById('root'));
